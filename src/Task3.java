4dc5d2ed29583b6d6b60d376546ab230f5858f91
André Schreck <moin@andreschreck.de>
2022-02-01 17:37:08 +0100

    week 1: task 3

---
+++ src/Task3.java
@@ -0,0 +1,13 @@
+public class Task3 {
+
+    public static void main(String[] args) {
+        var biggerNumber = 13;
+        var smallerNumber = 2;
+        var equalNumber = 13;
+
+        System.out.println(biggerNumber + " is bigger than " + smallerNumber + ": " + (biggerNumber > smallerNumber));
+        System.out.println(biggerNumber + " is smaller than " + smallerNumber + ": " + (biggerNumber < smallerNumber));
+        System.out.println(equalNumber + " is equal to " + equalNumber + ": " + (equalNumber != equalNumber));
+    }
+
+}
