faf08d78355c06a137c8e25a3ccafb0126897a6d
André Schreck <moin@andreschreck.de>
2022-02-01 15:59:12 +0100

    round

--- src/Task2.java
+++ src/Task2.java
@@ -6,6 +6,10 @@ public class Task2 {
         int sum = number1 + number2;
         System.out.println(sum);
         System.out.println("Summe " + sum);
+
+        double d1 = 4.8912;
+        double rounded = Math.round(d1*100.0) / 100.0;
+        System.out.println("Rounded: " + rounded);
     }
 
 }
