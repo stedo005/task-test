a2150606d39f1c4a13259f6280104e9e8140cd08
André Schreck <moin@andreschreck.de>
2022-02-07 13:59:01 +0100

    getter & setter

---
+++ myMavenProject/src/main/java/de/neuefische/objects/Student.java
@@ -0,0 +1,32 @@
+package de.neuefische.objects;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int semester;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+}
