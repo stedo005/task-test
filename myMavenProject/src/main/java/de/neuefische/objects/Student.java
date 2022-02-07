766fd0974e1a5d862250e4571a67c5df85259ece
André Schreck <moin@andreschreck.de>
2022-02-07 15:29:31 +0100

    constructor

--- myMavenProject/src/main/java/de/neuefische/objects/Student.java
+++ myMavenProject/src/main/java/de/neuefische/objects/Student.java
@@ -6,6 +6,20 @@ public class Student {
     private String lastName;
     private int semester;
 
+    Student(String firstName, String lastName, int semester) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.semester = semester;
+    }
+
+    Student(Student toCopy) {
+        this.firstName = toCopy.getFirstName();
+        this.lastName = toCopy.getLastName();
+        this.semester = toCopy.getSemester();
+    }
+
+    Student() {}
+
     public String getFirstName() {
         return firstName;
     }
